dcf6b83716428e07c7e5f69a2914309bfc4cf1de
jvjfranca <jvjfranca@gmail.com>
2020-12-12 06:53:45 -0300

    oo aula 5 getters e setters

--- conta.py
+++ conta.py
@@ -22,7 +22,26 @@ class Conta(object):
         self.sacar(valor)
         conta_destino.depositar(valor)
 
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
+    def get_limite(self):
+        return self.__limite
+
+    def get_numero(self):
+        return self.__numero
+
+    def set_limite(self, novo_limite):
+        self.__limite += novo_limite
+    
 
 if __name__ == "__main__":
     conta = Conta(123, "jose vicente", 8900.00)
+    conta.extrato()
+    conta.depositar(10)
+    conta.extrato()
+    conta.sacar(2)
     conta.extrato()
