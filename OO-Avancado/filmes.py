598e3fad7328dde697a70d126be68ba1ef2ab108
jvjfranca <jvjfranca@gmail.com>
2020-12-15 07:17:48 -0300

    oo avancado aula 2 - heranca

--- OO-Avancado/filmes.py
+++ OO-Avancado/filmes.py
@@ -1,69 +1,61 @@
-
-class Filme(object):
-    def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
-        self.ano = ano
-        self.duracao = duracao
-        self.__likes = 0
-
-    @property
-    def likes(self):
-        """
-        Retorna quantidade de likes
-        """
-        return self.__likes
-
-    def dar_likes(self):
-        """
-        Incrimenta likes
-        """
-        self.__likes += 1
+class Programa(object):
+    """
+    Classe Programa
+    """
+    _likes = 0
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self._ano = ano
 
     @property
     def nome(self):
         """
         Getter Nome
         """
-        return self.__nome
+        return self._nome
 
     @nome.setter
     def nome(self, nome):
         """
         Setter Nome
         """
-        self.__nome = nome
+        self._nome = nome.title()
+
+    @property
+    def ano(self):
+        """
+        Getter Ano
+        """
+        return self._ano
+
+    @ano.setter
+    def funcname(self, ano):
+        """
+        Setter Ano
+        """
+        self._ano = ano
+
+    def dar_likes(self):
+        """
+        Incrementa Likes
+        """
+        self._likes += 1
+
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
+        self.__duracao = duracao
 
 
-class Serie(object):
+class Serie(Programa):
     """
     Classe Serie
     """
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self.__likes = 0
 
-    @property
-    def likes(self):
-        """
-        Getter Likes
-        """
-        return self.__likes
-    
-    @property
-    def nome(self):
-        return self.__nome
 
-    def dar_likes(self):
-        """
-        Likes Setter
-        """
-        self.__likes += 1
-
-    @nome.setter
-    def nome(self, nome):
-        self.__nome = nome
 
 if __name__ == "__main__":
     vingadores = Filme('vingadores - guerra infinita', 2018, 160)
